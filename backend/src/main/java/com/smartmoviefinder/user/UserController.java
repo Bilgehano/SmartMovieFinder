f419e2bcaa5eaf7f443c12fae90520783f066744
Bilgehano <blghnoznvr@gmail.com>
2026-03-31 12:55:05 +0200

    add user controller

---
+++ backend/src/main/java/com/smartmoviefinder/user/UserController.java
@@ -0,0 +1,170 @@
+package com.smartmoviefinder.user;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.smartmoviefinder.movie.entity.MovieEntity;
+
+
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    @GetMapping("/test")
+    public String testEndpoint() {
+        return "UserController is working!";
+    }
+
+
+
+
+
+
+    @GetMapping
+    public List<UserEntity> getAllUsers() {
+    return userService.getAllUsers();
+    }
+
+
+    @GetMapping("/{id}")
+    public String getUserById(Long id) {
+        UserEntity user = userService.getUserById(id);
+        if (user != null) {
+            return "User found: " + user.getUsername();
+        } else {
+            return "User not found with ID: " + id;
+        }
+    }
+
+
+    @GetMapping("/username/{username}")
+    public String getUserByUsername(String username) {
+        UserEntity user = userService.getUserByUsername(username);
+        if (user != null) {
+            return "User found: " + user.getUsername();
+        } else {
+            return "User not found with username: " + username;
+        }
+    }
+
+
+
+
+
+
+    @PostMapping
+    public UserEntity createUser(UserEntity user) {
+        return userService.createUser(user);
+    }
+
+
+    //Add watched movie
+    @PostMapping("/{userId}/watched/{movieId}")
+    public void addWatchedMovie(Long userId, Long movieId) {
+        userService.addWatchedMovie(userId, movieId);
+    }
+
+    //Add favorite genre
+    @PostMapping("/{userId}/favorite-genre/{genreId}")
+    public void addFavoriteGenre(Long userId, Long genreId) {
+        userService.addFavoriteGenre(userId, genreId);
+    }
+
+    //Rate a movie
+    @PostMapping("/{userId}/rate/{movieId}/{rating}")
+    public void rateMovie(Long userId, Long movieId, int rating) {
+        userService.rateMovie(userId, movieId, rating);
+    }
+
+
+
+
+
+
+
+
+
+
+    @GetMapping("/exists/id/{id}")
+    public boolean userExistsById(Long id) {
+        return userService.userExistsById(id);
+    }
+
+    @GetMapping("/exists/username/{username}")
+    public boolean userExistsByUsername(String username) {
+        return userService.userExistsByUsername(username);
+    }
+
+    @GetMapping("/exists/email/{email}")
+    public boolean userExistsByEmail(String email) {    
+        return userService.userExistsByEmail(email);
+    }
+
+
+
+    //Get watched movies for a user
+    @GetMapping("/{userId}/watched")
+    public List<MovieEntity> getWatchedMovies(@PathVariable Long userId) {
+        return userService.getWatchedMovies(userId);
+    }
+
+    //Get favorite genres for a user
+    @GetMapping("/{userId}/favorite-genres")
+    public List<String> getFavoriteGenres(@PathVariable Long userId) {
+        return userService.getFavoriteGenres(userId);
+    }
+
+    //Get movie ratings for a user
+    @GetMapping("/{userId}/ratings")    
+    public List<String> getMovieRatings(@PathVariable Long userId) {
+        return userService.getMovieRatings(userId);
+    }
+
+
+    //Delete a user
+    @DeleteMapping("/{id}")
+    public void deleteUser(Long id) {
+        userService.deleteUser(id);
+    }
+
+
+    //Delete watched movie
+    @DeleteMapping("/{userId}/watched/{movieId}")
+    public void deleteWatchedMovie(Long userId, Long movieId) {
+        userService.deleteWatchedMovie(userId, movieId);
+    }
+
+    //Delete favorite genre
+    @DeleteMapping("/{userId}/favorite-genre/{genreId}")
+    public void deleteFavoriteGenre(Long userId, Long genreId) {
+        userService.deleteFavoriteGenre(userId, genreId);
+    }
+
+    //Delete movie rating
+    @DeleteMapping("/{userId}/rate/{movieId}")
+    public void deleteMovieRating(Long userId, Long movieId) {  
+        userService.deleteMovieRating(userId, movieId);
+    }
+
+
+
+
+
+
+}
