63d16e02fa1071e30784589095b55339d524e809
Fabian Wottke <fabian.wottke@t-online.de>
2026-06-06 17:08:28 +0200

    Merge main into Testing

--- backend/catalog-service/src/main/java/com/smartmoviefinder/catalogservice/movie/MovieController.java
+++ backend/catalog-service/src/main/java/com/smartmoviefinder/catalogservice/movie/MovieController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +16,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.smartmoviefinder.catalogservice.integration.TmdbClient;
 
 @RestController
-@RequestMapping("/movies")
+@RequestMapping(value = "/movies", produces = MediaType.APPLICATION_JSON_VALUE)
 public class MovieController {
 
     private final TmdbClient tmdbClient;
