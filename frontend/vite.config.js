a2be0037dd3ae847ad0039eb26f748d9291c725a
Bilgehano <blghnoznvr@gmail.com>
2026-06-29 18:16:54 +0000

    Node modules installed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // Füge das hier hinzu, um Blobs zu erlauben:
+    headers: {
+      "Content-Security-Policy": "script-src 'self' 'unsafe-inline' 'unsafe-eval' blob:;"
+    },
+    proxy: {
+      '/api': {
+        target: 'http://193.197.230.150:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
